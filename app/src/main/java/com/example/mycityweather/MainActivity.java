607d164642d00465658efbb639ea12d7c20c3d09
Chintan Patel <chintan97patel@gmail.com>
2019-03-17 12:02:22 -0300

    App with city exception handling

--- app/src/main/java/com/example/mycityweather/MainActivity.java
+++ app/src/main/java/com/example/mycityweather/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mycityweather;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
@@ -16,6 +18,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.NetworkError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -47,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     private AutoCompleteTextView get_city_name;
     List<String> city_data;
     ArrayAdapter<String> city_adapter;
+    LinearLayout layout_for_error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         show_cloud = findViewById(R.id.show_cloud);
         show_data_layout = findViewById(R.id.show_data_layout);
         show_data_layout.setVisibility(View.INVISIBLE);
+        layout_for_error = findViewById(R.id.layout_show_error);
+        layout_for_error.setVisibility(View.INVISIBLE);
 
         city_data = new ArrayList<String>();
         try{
@@ -141,51 +147,54 @@ public class MainActivity extends AppCompatActivity {
 
                         // Now store parsed data
                         try {
-                            city_name = response.getString("name");
-                            JSONObject main = response.getJSONObject("main");
-                            current_temperature = main.getDouble("temp");
-                            min_temperature = main.getDouble("temp_min");
-                            max_temperature = main.getDouble("temp_max");
-                            JSONArray weather = response.getJSONArray("weather");
-                            //JSONArray weather_list = weather.getJSONArray("weather");
-                            JSONObject main_object = weather.getJSONObject(0);
-                            JSONObject description_object = weather.getJSONObject(0);
-                            main_weather = main_object.getString("main");
-                            weather_description = description_object.getString("description");
-                            humidity = main.getDouble("humidity");
-                            JSONObject clouds = response.getJSONObject("clouds");
-                            cloud_coverage = clouds.getDouble("all");
+                             city_name = response.getString("name");
+                             JSONObject main = response.getJSONObject("main");
+                             current_temperature = main.getDouble("temp");
+                             min_temperature = main.getDouble("temp_min");
+                             max_temperature = main.getDouble("temp_max");
+                             JSONArray weather = response.getJSONArray("weather");
+                             //JSONArray weather_list = weather.getJSONArray("weather");
+                             JSONObject main_object = weather.getJSONObject(0);
+                             JSONObject description_object = weather.getJSONObject(0);
+                             main_weather = main_object.getString("main");
+                             weather_description = description_object.getString("description");
+                             humidity = main.getDouble("humidity");
+                             JSONObject clouds = response.getJSONObject("clouds");
+                             cloud_coverage = clouds.getDouble("all");
 
-                            WeatherData_model model = new WeatherData_model(city_name, current_temperature, min_temperature,
-                                    max_temperature, main_weather, weather_description, humidity, cloud_coverage);
+                             WeatherData_model model = new WeatherData_model(city_name, current_temperature, min_temperature,
+                                     max_temperature, main_weather, weather_description, humidity, cloud_coverage);
+                             weatherItems.add(model);
 
-                            weatherItems.add(model);
+                             String show_temp = String.format("%.2f", model.getCurrent_temperature() - 273.15) + (char) 0x00B0 + "C";
+                             String min_temp = String.format("%.2f", model.getMin_temperature() - 273.15) + (char) 0x00B0 + "C";
+                             String max_temp = String.format("%.2f", model.getMax_temperature() - 273.15) + (char) 0x00B0 + "C";
+                             String humidity_string = String.valueOf(model.getHumidity()) + "%";
+                             String cloud_string = String.valueOf(model.getCloud_coverage()) + "%";
+                             show_city_name.setText(model.getCity_name());
+                             show_main_temp.setText(show_temp);
+                             show_min_temp.setText(min_temp);
+                             show_max_temp.setText(max_temp);
+                             show_main_weather.setText(model.getMain_weather());
+                             show_description.setText(model.getWeather_description());
+                             show_humidity.setText(humidity_string);
+                             show_cloud.setText(cloud_string);
 
-                            String show_temp = String.format("%.2f", model.getCurrent_temperature() - 273.15) + (char) 0x00B0 + "C";
-                            String min_temp = String.format("%.2f", model.getMin_temperature() - 273.15) + (char) 0x00B0 + "C";
-                            String max_temp = String.format("%.2f", model.getMax_temperature() - 273.15) + (char) 0x00B0 + "C";
-                            String humidity_string = String.valueOf(model.getHumidity()) + "%";
-                            String cloud_string = String.valueOf(model.getCloud_coverage()) + "%";
-                            show_city_name.setText(model.getCity_name());
-                            show_main_temp.setText(show_temp);
-                            show_min_temp.setText(min_temp);
-                            show_max_temp.setText(max_temp);
-                            show_main_weather.setText(model.getMain_weather());
-                            show_description.setText(model.getWeather_description());
-                            show_humidity.setText(humidity_string);
-                            show_cloud.setText(cloud_string);
-
-                            show_data_layout.setVisibility(View.VISIBLE);
-                            Log.i("city name", model.getWeather_description());
-
+                             layout_for_error.setVisibility(View.INVISIBLE);
+                             show_data_layout.setVisibility(View.VISIBLE);
+                             Log.i("city name", model.getWeather_description());
                         } catch (JSONException e){
                             e.printStackTrace();
+                        } catch (Exception e){
+                            layout_for_error.setVisibility(View.VISIBLE);
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "Could not fetch data!", Toast.LENGTH_SHORT).show();
+                layout_for_error.setVisibility(View.VISIBLE);
+                show_data_layout.setVisibility(View.INVISIBLE);
+                //Toast.makeText(getApplicationContext(), "Could not fetch data!", Toast.LENGTH_SHORT).show();
             }
         });
 
